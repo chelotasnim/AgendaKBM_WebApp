b3edd503e0c48cf8c4be0ccd1ef90be13cf54c5f
chelotasnim <grumpfyxylozx@gmail.com>
2023-09-05 10:36:06 +0700

    Schedule Impor Feature

--- app/Imports/JadwalImport.php
+++ app/Imports/JadwalImport.php
@@ -2,12 +2,16 @@
 
 namespace App\Imports;
 
+use App\Models\GuruMapel;
+use App\Models\Jadwal;
+use App\Models\Kelas;
+use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 
 class JadwalImport implements ToModel, WithStartRow
 {
-    private $hari;
+    protected $hari;
 
     public function __construct($hari)
     {
@@ -25,5 +29,72 @@ class JadwalImport implements ToModel, WithStartRow
      */
     public function model(array $row)
     {
+        $validator = Validator::make($row, [
+            0 => 'required',
+            1 => '',
+            2 => 'required',
+            3 => 'required',
+            4 => 'required',
+            5 => 'required',
+            6 => 'required',
+            7 => 'required',
+            8 => 'required',
+            9 => 'required',
+            10 => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return null;
+        };
+
+        $id_jam = 2;
+
+        if ($this->hari == 'Senin') {
+            $id_jam = 1;
+        };
+
+        $split_kelas = explode(' ', $row[0]);
+        $get_jenjang = $split_kelas[0];
+        $get_kelas = str_replace($get_jenjang . ' ', '', $row[0]);
+        $data_kelas = Kelas::whereHas('jenjang', function ($query) use ($get_jenjang) {
+            $query->where('jenjang', $get_jenjang);
+            $query->where('hidden', 0);
+            $query->where('status', 1);
+        })->with('jenjang')->where('name', $get_kelas)->where('hidden', 0)->where('status', 1)->select('id')->first();
+
+
+        if ($data_kelas != null) {
+            for ($i = 1; $i <= count($row) - 1; $i++) {
+                if ($row[$i] != '') {
+                    $kode_guru = explode(',', $row[$i])[0];
+                    $kode_mapel = null;
+                    if (count(explode(',', $row[$i])) > 1) {
+                        $kode_mapel = explode(',', $row[$i])[1];
+                    };
+
+                    $guru_mapel = array();
+                    if ($kode_mapel != null) {
+                        $guru_mapel = GuruMapel::whereHas('guru', function ($query) use ($kode_guru) {
+                            $query->where('kode', $kode_guru);
+                        })->where('guru_mapel', $kode_mapel)->first();
+                    } else {
+                        $guru_mapel = GuruMapel::whereHas('guru', function ($query) use ($kode_guru) {
+                            $query->where('kode', $kode_guru);
+                        })->first();
+                    };
+
+                    if ($guru_mapel != null) {
+                        Jadwal::create([
+                            'kelas_id' => $data_kelas->id,
+                            'hari' => $this->hari,
+                            'jam_id' => $id_jam,
+                            'guru_mapel_id' => $guru_mapel->id
+                        ]);
+                    };
+                };
+            };
+        };
+
+        return null;
     }
 }
